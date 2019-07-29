69bc68369ab59d197773f6cdf82d89e82f6222a9
JasperZhao97 <zhaolei97@foxmail.com>
2019-07-29 19:19:37 +0800

    Create Ishape.java

---
+++ code/JavaSE/demo20190729/code0729/src/com/implementtest/Ishape.java
@@ -0,0 +1,19 @@
+package com.implementtest;
+
+/**
+ * 定义的接口类
+ */
+public interface Ishape {
+
+    /**
+     * 计算图形的周长
+     * @return
+     */
+    public double calculatorPerimeter();
+
+    /**
+     * 计算图形的面积
+     * @return
+     */
+    public double calculatorArea();
+}
